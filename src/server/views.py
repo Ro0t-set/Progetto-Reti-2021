cb9f8ff87c39d31a8673eba3a44fd1c432a7f647
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2021-05-06 12:36:29 +0200

    user visualization

--- src/server/views.py
+++ src/server/views.py
@@ -1,6 +1,10 @@
-import random
+import base64
+
+
 def index(request, page):
-    return page.format(username= request.headers.get('Authorization').split(' ')[0])
+    username = str(base64.b64decode(request.headers.get('Authorization').split(' ')[1])).split(':')[0][2:]
+    print(username)
+    return page.format(username=username)
 
 
 def login(request):
